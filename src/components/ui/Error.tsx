7f7799b0230f91160f4d2c21a12eccb36cff7f09
AmtTawsik <amttawsik.cse@gmail.com>
2023-07-16 17:19:25 +0600

    Edit some code in Error

--- src/components/ui/Error.tsx
+++ src/components/ui/Error.tsx
@@ -3,7 +3,7 @@ import LinkButton from './LinkButton';
 function Error({ message }: { message: string }) {
   return (
     <div>
-      <h1>Something went wrong 😢</h1>
+      <h1>Something went wrong</h1>
       <p>{message}</p>
       <LinkButton to="-1">&larr; Go back</LinkButton>
     </div>
